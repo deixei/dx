77423cc5c7cf3742c54c4d40e06b692044565e99
Marcio Parente <marcio.parente@swisslife-am.com>
2024-02-28 11:03:54 +0000

    az updates

--- scripts/azcli.sh
+++ scripts/azcli.sh
@@ -3,6 +3,14 @@ subcommand="az"
 script_dir=$(dirname "$0")
 source $script_dir/common.sh
 
+
+az_output_arg="json" # default output format: json, yaml, table
+
+documents() {
+    print_warning "### DX Documents - $subcommand ###"
+    echo "https://learn.microsoft.com/en-us/cli/azure/reference-docs-index"
+}
+
 usage() {
   print_warning "### DX tools - $subcommand ###"
   echo
@@ -36,6 +44,51 @@ cmd_az_set_subscription() {
     az account set --subscription "$subscription_id"
 }
 
+cmd_list_subscriptions() {
+    az account list --query "[].{subscription_id:id, name:name, isDefault:isDefault}" -o table
+}
+
+cmd_list_resource_groups_by_name() {
+    local resource_group_name="$1"
+
+    az group list --query "[? contains(name, '$resource_group_name')].{ResourceGroupName:name, ResourceGroupId:id}" -o table
+}
+
+cmd_search_resource_groups_by_name() {
+    local resource_group_name="$1"
+
+    # Get the list of subscriptions
+    subscriptions=$(az account list --query "[].{Name:name, Id:id}" --output tsv)
+
+    # Loop over each subscription
+    while IFS=$'\t' read -r -a subscription
+    do
+        # Set the subscription for the az command
+        print_info "Searching in subscription: ${subscription[0]}"
+        az account set --subscription "${subscription[1]}"
+
+        # List the resource groups in the subscription
+        az group list --query "[? contains(name, '$resource_group_name')].{ResourceGroupName:name, ResourceGroupId:id, SubscriptionName:'${subscription[0]}'}" -o table
+    done <<< "$subscriptions"
+}
+
+cmd_az_rest() {
+    local method="$1"
+    local url="$2"
+    local body="$3"
+    
+    # Get the access token
+    local access_token=$(az account get-access-token --output json | jq -r .accessToken)
+
+    # Include the access token in the Authorization header
+    az rest --method "$method" --uri "$url" --body "$body" --headers "Authorization=Bearer $access_token"
+
+}
+
+get_applications() {
+    cmd_az_rest "GET" "https://graph.microsoft.com/v1.0/me" ""
+}
+
 
 main() {
     # Parse command line options
@@ -45,10 +98,14 @@ main() {
           usage
           exit 0
           ;;
+        -o|--output)
+          az_output_arg=$2
+          shift
+          ;;
         -n|--name)
           name_arg=$2
           shift
-          ;;
+          ;;          
         *)
           command=$1
           ;;
@@ -65,21 +122,39 @@ main() {
     # Execute the command
     case $command in
         show)
-          shift
-          command_show
-
-          ;;
-
-        new)
+            shift
+            command_show
+            ;;
+        login)
+            shift
+            cmd_az_login
+            ;;
+        ls)
+            shift
+            cmd_list_subscriptions
+            ;;
+        lrg)
           shift
           if [[ -z "$name_arg" ]]; then
               print_error "Error: Missing name argument (--name or -n)"
               exit 1
           fi
 
-          echo "$name_arg"
+          cmd_list_resource_groups_by_name "$name_arg"
           ;;
+        searchrg)
+          shift
+          if [[ -z "$name_arg" ]]; then
+              print_error "Error: Missing name argument (--name or -n)"
+              exit 1
+          fi
 
+          cmd_search_resource_groups_by_name "$name_arg"
+          ;;
+          apps)
+            shift
+            get_applications
+            ;;
         *)
             print_error "Error: [$command] Unsupported command"
             usage
