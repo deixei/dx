dab02fd888b11b01e9cc1ca1faf5309efa5612c4
Marcio <marcio@deixei.com>
2024-03-28 07:54:43 +0100

    cc in

--- scripts/cookiecutter.sh
+++ scripts/cookiecutter.sh
@@ -12,13 +12,53 @@ usage() {
   echo
   print_info "Options:"
   echo "  -h, --help      Show this help message and exit"
+  echo "  -n, --name      Name of the template"
+  echo "  -i, --input     Input file"
   echo
   print_info "Commands:"
+  echo
+  echo "  show                                Show information"
+  echo "  init -n <name> -i user_config.yaml  Initialize a new template"
+  echo
+  print_info "Examples:"
+  echo
+  echo " dx cc init -n simple -i user_config.yaml"
 
 }
 
 # cookiecutter https://github.com/user/repo-name.git --directory="directory1-name"
 # cookiecutter https://github.com/deixei/cookie.git --directory="simple"
+
+command_run() {
+  local directory_name=$1
+  local input_file=$2
+  print_info "Running cookiecutter"
+  # load the configuration
+  load_config
+
+    # check that cookiecutter is installed
+    if ! command -v cookiecutter &> /dev/null
+    then
+        print_info "cookiecutter is not installed. Run: dx install cookiecutter"
+    else
+      # if no input file is provided
+      if [[ -z "$input_file" ]]; then
+        cookiecutter https://github.com/deixei/cookie.git --directory="$directory_name"
+      else
+
+        # if input file exists
+        if [ ! -f "$input_file" ]; then
+          print_error "Error: Input file not found: $input_file"
+          exit 1
+        fi
+
+        cookiecutter https://github.com/deixei/cookie.git --directory="$directory_name" --no-input --config-file "$input_file"
+      fi
+      
+    fi
+
+}
+
 
 command_show() {
   print_info "Showing things"
@@ -40,6 +80,10 @@ main() {
           name_arg=$2
           shift
           ;;
+        -i|--input)
+          input_arg=$2
+          shift
+          ;;
         *)
           command=$1
           ;;
@@ -58,17 +102,15 @@ main() {
         show)
           shift
           command_show
-
           ;;
-
-        new)
+        init)
           shift
           if [[ -z "$name_arg" ]]; then
               print_error "Error: Missing name argument (--name or -n)"
               exit 1
           fi
 
-          echo "$name_arg"
+          command_run "$name_arg" "$input_arg"
           ;;
 
         *)
